cdeecdc7015ac6158e67733bc36c6ac630dbf703
ireneli0823 <mingcanli1998@gmail.com>
2025-02-11 21:01:20 -0500

    Initial Commit

---
+++ tomato_timer/tomato_timerApp.swift
@@ -0,0 +1,17 @@
+//
+//  tomato_timerApp.swift
+//  tomato_timer
+//
+//  Created by Mingcan Li on 2/11/25.
+//
+
+import SwiftUI
+
+@main
+struct tomato_timerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ tomato_timerTests/tomato_timerTests.swift
@@ -0,0 +1,16 @@
+//
+//  tomato_timerTests.swift
+//  tomato_timerTests
+//
+//  Created by Mingcan Li on 2/11/25.
+//
+
+import Testing
+
+struct tomato_timerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
